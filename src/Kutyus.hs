8b03a324772324bc36e2ab7e8c72b369b4901602
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2017-12-24 14:36:09 +0100

    started frame parsing

--- src/Kutyus.hs
+++ src/Kutyus.hs
@@ -1,16 +1,21 @@
 module Kutyus
     ( Frame
     , Message
+    , unpackFrame
+    , UnpackError(..)
+    , BaseFrame
     ) where
 
+import qualified Data.MessagePack as MP
 import qualified Data.ByteString.Lazy as B
 
-newtype MessageId = MessageId B.ByteString
+newtype MessageId = MessageId B.ByteString deriving (Eq, Show)
 
-newtype AuthorId = AuthorId B.ByteString
+newtype AuthorId = AuthorId B.ByteString deriving (Eq, Show)
 
 data ContentType =
     Blob
+    deriving (Eq, Show)
 
 type BaseMessage = Message B.ByteString
 
@@ -19,10 +24,17 @@ data Message a = Message
   , parent :: !(Maybe MessageId)
   , content_type :: !ContentType
   , content :: !a
-  }
+  } deriving (Eq, Show)
+
+type BaseFrame = Frame B.ByteString
 
 data Frame a = Frame
   { version :: !Int
   , message :: Message a
   , signature :: !B.ByteString
-  }
+  } deriving (Eq, Show)
+
+data UnpackError = InvalidVersion | FrameFormatError | MessageFormatError | InvalidSignature deriving (Eq, Show)
+
+unpackFrame :: B.ByteString -> Either UnpackError (Frame a)
+unpackFrame _ = Left InvalidVersion
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.MessagePack as MP
+import qualified Data.ByteString.Lazy as B
+
+import Test.Hspec
+
+import Kutyus
+
+frameSpec :: Spec
+frameSpec =
+  describe "frames" $ do
+    it "fails to unpack messages with versions oher than 1" $ do
+      let messageWithInvalidVersion = MP.pack (0::Int, "message"::B.ByteString, "signature"::B.ByteString) :: B.ByteString
+      let maybeUnpackedMessage = unpackFrame messageWithInvalidVersion :: Either UnpackError BaseFrame
+      maybeUnpackedMessage `shouldBe` (Left InvalidVersion)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  frameSpec
