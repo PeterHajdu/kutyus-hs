b8aeb1437a83ea706fc2db100ab03a1ce89ce0f6
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2017-12-24 13:59:40 +0100

    add first version of Message type

--- src/Kutyus.hs
+++ src/Kutyus.hs
@@ -1,13 +1,26 @@
 module Kutyus
     ( Frame
+    , Message
     ) where
 
 import qualified Data.ByteString.Lazy as B
 
+newtype MessageId = MessageId B.ByteString
+
+newtype AuthorId = AuthorId B.ByteString
+
+data ContentType =
+    Blob
+
 data Message a = Message
+  { author :: !AuthorId
+  , parent :: !(Maybe MessageId)
+  , content_type :: !ContentType
+  , content :: !B.ByteString
+  }
 
 data Frame a = Frame
-  { version :: Int
+  { version :: !Int
   , message :: Message a
-  , signature :: B.ByteString
+  , signature :: !B.ByteString
   }
