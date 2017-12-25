c58246a18632d8dbe967206a248f1c670bf62004
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2017-12-25 11:37:52 +0100

    parse message with dummy signature test

--- src/Kutyus.hs
+++ src/Kutyus.hs
@@ -9,6 +9,7 @@ module Kutyus
 import qualified Data.MessagePack as MP
 import qualified Data.ByteString.Lazy as B
 import Control.Monad
+import Data.Maybe
 
 newtype MessageId = MessageId B.ByteString deriving (Eq, Show)
 
@@ -43,8 +44,8 @@ maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither _ (Just val) = Right val
 maybeToEither err Nothing = Left err
 
-unpackFrame :: B.ByteString -> Either UnpackError (Frame B.ByteString)
-unpackFrame = unpackRawFrame >=> checkVersion >=> undefined
+unpackFrame :: B.ByteString -> Either UnpackError BaseFrame
+unpackFrame = unpackRawFrame >=> checkVersion >=> unpackMessage >=> checkSignature >=> undefined
 
 unpackRawFrame :: B.ByteString -> Either UnpackError RawFrame
 unpackRawFrame buffer = let maybeRawFrame = MP.unpack buffer :: Maybe RawFrame
@@ -54,3 +55,18 @@ checkVersion :: RawFrame -> Either UnpackError RawFrame
 checkVersion frame@(1, _, _) = Right frame
 checkVersion (_, _, _) = Left InvalidVersion
 
+type RawMessage = (B.ByteString, [B.ByteString], B.ByteString, B.ByteString)
+unpackMessage :: RawFrame -> Either UnpackError BaseFrame
+unpackMessage (version, message, signature) = let maybeRawMessage = MP.unpack message :: Maybe RawMessage
+                                                  maybeBaseMessage = unpackRawMessage <$> maybeRawMessage
+                                                  maybeBaseFrame = (\msg -> Frame version msg signature) <$> maybeBaseMessage
+                                               in maybeToEither MessageFormatError maybeBaseFrame
+
+unpackRawMessage :: RawMessage -> BaseMessage
+unpackRawMessage (author, parent, contentType, content) = Message (AuthorId author) (MessageId <$> listToMaybe parent) (parseContentType contentType) content
+
+parseContentType :: B.ByteString -> ContentType
+parseContentType _ = Blob
+
+checkSignature :: BaseFrame -> Either UnpackError BaseFrame
+checkSignature _ = Left InvalidSignature
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,15 +10,26 @@ import Kutyus
 frameSpec :: Spec
 frameSpec =
   describe "frames" $ do
-    it "fails to unpack messages with versions oher than 1" $ do
+    it "fails to unpack frames with versions oher than 1" $ do
       let messageWithInvalidVersion = MP.pack (0::Int, "message"::B.ByteString, "signature"::B.ByteString) :: B.ByteString
       let maybeUnpackedMessage = unpackFrame messageWithInvalidVersion :: Either UnpackError BaseFrame
       maybeUnpackedMessage `shouldBe` (Left InvalidVersion)
 
-    it "fails to unpack messages with wrong format" $ do
-      let messageWithInvalidVersion = MP.pack ("message"::B.ByteString, "signature"::B.ByteString) :: B.ByteString
-      let maybeUnpackedMessage = unpackFrame messageWithInvalidVersion :: Either UnpackError BaseFrame
+    it "fails to unpack frames with wrong format" $ do
+      let messageWithInvalidFrame = MP.pack ("message"::B.ByteString, "signature"::B.ByteString) :: B.ByteString
+      let maybeUnpackedMessage = unpackFrame messageWithInvalidFrame :: Either UnpackError BaseFrame
       maybeUnpackedMessage `shouldBe` (Left FrameFormatError)
+
+    it "fails to unpack messages with wrong format" $ do
+      let messageWithInvalidMessage = MP.pack (1::Int, "invalidmessage"::B.ByteString, "signature"::B.ByteString) :: B.ByteString
+      let maybeUnpackedMessage = unpackFrame messageWithInvalidMessage :: Either UnpackError BaseFrame
+      maybeUnpackedMessage `shouldBe` (Left MessageFormatError)
+
+    it "fails to unpack messages with an invalid signature" $ do
+      let validMessage = MP.pack ("authur"::B.ByteString, []::[B.ByteString], "\0"::B.ByteString, "example"::B.ByteString)
+      let messageWithInvalidSignature = MP.pack (1::Int, validMessage, "invalid signature"::B.ByteString) :: B.ByteString
+      let maybeUnpackedMessage = unpackFrame messageWithInvalidSignature :: Either UnpackError BaseFrame
+      maybeUnpackedMessage `shouldBe` (Left InvalidSignature)
 
 main :: IO ()
 main = hspec $ do
