f5af638ca90d4c250399afd7f1ff08432179f3a4
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2018-01-07 06:45:38 +0100

    rewrite vau using the network submodule

--- app/Vau.hs
+++ app/Vau.hs
@@ -1,33 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Kutyus
-import Network.Socket (withSocketsDo, sendTo, recvFrom)
-import Network.Multicast
+import Control.Concurrent.STM.TChan
+import Control.Concurrent.STM
 import Control.Monad (forever)
 import Control.Concurrent (forkIO)
+import qualified Data.ByteString.Lazy as L
+import Data.ByteString.Lazy.Char8 (pack)
 
-publish :: String -> IO ()
-publish message = do
-  withSocketsDo $ do
-    (sock, addr) <- multicastSender "224.0.0.99" 9999
-    sendTo sock message addr
-    putStrLn $ "message sent: " ++ message
+startReceiver :: IO ()
+startReceiver = do
+  receiverChannel <- newTChanIO
+  forkIO $ startBroadcastReceiver receiverChannel
+  forkIO $ startPrinter receiverChannel
   pure ()
 
-repl :: (String -> IO ()) -> IO ()
-repl handler = forever $ do
-  putStrLn "> "
-  line <- getLine
-  handler line
+startPrinter :: BroadcastChannel -> IO ()
+startPrinter chan = forever $ do
+  msg <- atomically $ readTChan chan
+  L.putStrLn msg
 
-receiver :: IO ()
-receiver = do
-  sock <- multicastReceiver "224.0.0.99" 9999
-  forever $ do
-    (msg, _, addr) <- recvFrom sock 1024
-    putStrLn $ "Recv [" ++ show addr ++ "]: " ++ msg
+startSender :: IO BroadcastChannel
+startSender = do
+  senderChannel <- newTChanIO
+  forkIO $ startBroadcastSender senderChannel
+  pure senderChannel
+
+repl :: (L.ByteString -> IO ()) -> IO ()
+repl publisher = forever $ do
+  putStrLn ">"
+  line <- getLine
+  publisher (pack line)
+
+broadcastMessage :: BroadcastChannel -> L.ByteString -> IO ()
+broadcastMessage chan msg = atomically $ writeTChan chan msg
 
 main :: IO ()
 main = do
-  forkIO receiver
-  repl publish
+  startReceiver
+  senderChannel <- startSender
+  repl (broadcastMessage senderChannel)
+
